b582cf173aaa41d41a2d955e33b14022df748222
vivekamin <vivek@yodaplus.com>
2017-02-03 17:04:39 +0530

    brach into develope and modified server

--- server/generate.js
+++ server/generate.js
@@ -1,12 +1,20 @@
 module.exports = function () {
     var faker = require("faker");
     var _ = require("lodash");
+    var faker = require("faker/locale/en_IND")
     return {
-        people: _.times(100, function(n){
+        people: _.times(10, function(n){
             return {
                 id : n,
                 name : faker.name.findName(),
-                avatar : faker.internet.avatar()
+                avatar : faker.internet.avatar(),
+                email : faker.internet.email(),
+                phoneNumber : faker.phone.phoneNumber(),
+                company : faker.company.companyName(),
+                jobTitle : faker.name.jobTitle(),
+                jobArea : faker.name.jobArea(),
+                jobDesc : faker.name.jobDescriptor(),
+                address : faker.address.*()
             }
         })
 
